908ac3f67ffb0196621b83e9b6a016c68d5f91a1
Miguel Banuelos <mistergengar@gmail.com>
2015-04-24 09:05:58 -0700

    head front

--- js/entities/MiniPlayerLoacation.js
+++ js/entities/MiniPlayerLoacation.js
@@ -8,11 +8,33 @@ game.MiniPlayerLocation = me.Entity.extend({
        this.settings.height = this.diameter;
        this.settings.spritewidth = this.diameter;
        this.settings.spriteheight = this.diameter;
+       this.settings = this.diameter;
        this.floating = true;
+       this.image = me.video.createCanvas(this.settings.width, this.settings.height);
+       var ctx = me.video.renderer.getContext2d(this.image);
+       
+       ctx.fillstyle = "rgba(0, 192, 32, 0.75)";
+       ctx.strokeStyle = "blue";
+       ctx.linewidth = 2;
+       
+       ctx.arc(this.r + 2, this.r + 2, this.r, 0, Math.PI*2);
+       ctx.fill();
+       ctx.stroke();
+       
+       var my = this;
+       this._super(me.Entity, "init", [x, y, {
+           width: my.diamension,
+           height: my.diamension,
+           spritewidth: my.diamension,
+           spriteheight: my.diamension,
+           getShape: function(){
+                 return(new me.Rect(0, 0, my.diamension, my.diamension)).toPolygon();   
+           }              
+       }]);
    },
    
    draw: function(){
-       
+       this._super(me.Entity, "draw", [renderer])
    },
    
    update: function(){

709c22193ee09e3eb04af3dd4c4af9ea12a5fcc9
Miguel Banuelos <mistergengar@gmail.com>
2015-04-28 09:18:41 -0700

    mkmmkmmmmmmmm

--- js/entities/MiniPlayerLoacation.js
+++ js/entities/MiniPlayerLoacation.js
@@ -46,7 +46,6 @@ game.MiniPlayerLocation = me.Entity.extend({
    update: function(){
        this.pos.x = (10 + (game.data.player.pos.x * 0.12));
        this.pos.y = (10+(game.data.player.pos.y * 0.12));
-       console.log(game.data.player.pos.x + game.data.player.pos.y "");
        return true;
    }
 
--- js/screens/GameOver.js
+++ js/screens/GameOver.js
@@ -20,7 +20,7 @@ game.GameOverScreen = me.ScreenObject.extend({
                 })));
           this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
              if(action === "start"){
-                 me.state.change(me.state.PLAY);
+                 me.state.change(me.state.SPENDEXP);
              } 
           });
           
